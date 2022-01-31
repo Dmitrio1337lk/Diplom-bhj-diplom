d9b56dd7fd55009153097d0c3f137dc8d6429f81
Dmitrio <ar-dimasik@ya.ru>
2022-01-31 19:02:16 +0100

    FIX

--- public/js/api/Transaction.js
+++ public/js/api/Transaction.js
@@ -4,6 +4,16 @@
  * Имеет свойство URL со значением '/transaction'
  * */
  class Transaction extends Entity {
-    static URL = "/transaction";
+  static URL = "/transaction";
+
+  static list(data, callback){
+    createRequest({
+      url: this.URL + '?account_id='+ data.account_id,
+      method: 'GET',
+      responseType: 'json',
+      data,
+      callback,
+    });
   }
+}
   
